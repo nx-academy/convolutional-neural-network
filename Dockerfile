516bcbf1ac5caeb6bd9d83efaf1d9c42fca52867
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-12 10:12:48 +0200

    Setting Up libraries:
    - Installing required libraries such as tensorflow and pandas
    - Using DevContainer instead of full container (in order to have VSCode
      intellisense)

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ FROM python:3.11
 WORKDIR /app
 COPY . .
 
-
-
+RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import tensorflow as tf
+# from tensorflow.python.keras import layers, models, datasets
+from tensorflow.
+
+import tensorflow_datasets as tfds
+
 def main():
-    print("====")
+    print("=====")
 
-main()
 
+if __name__ == "__main__":
+    main()
