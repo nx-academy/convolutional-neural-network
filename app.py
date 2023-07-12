d40c9b8e70c3799b30f9506219f320258a74c63d
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-07-12 10:40:34 +0200

    Adding jupyter and downloading dataset

--- app.py
+++ app.py
@@ -7,7 +7,15 @@ from keras import layers, models, datasets
 
 import tensorflow_datasets as tfds
 
+ds, ds_info = tfds.load('food101', shuffle_files=True, as_supervised=True, with_info=True)
+
+train_ds, valid_ds = ds["train"], ds["validation"]
+
+fig = tfds.show_examples(train_ds, ds_info)
+
 def main():
+    print("=====")
+    print(fig)
     print("=====")
 
 
